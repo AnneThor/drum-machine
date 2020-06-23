e1107a3f304743cd7d7e0ceb67dd58d29affa73f
Anne Thor <anne.thor@gmail.com>
2020-06-22 23:50:27 -0700

    Updates

---
+++ precache-manifest.7cdc5d63a8e79441cd439e5f1585e58d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a952c1e8b5d1fcc9ff112af595fe2840",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "e6c5a8b0f89450342536",
+    "url": "/drum-machine/static/css/main.014aa4cb.chunk.css"
+  },
+  {
+    "revision": "58baceee2a770246d764",
+    "url": "/drum-machine/static/js/2.767ac253.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drum-machine/static/js/2.767ac253.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6c5a8b0f89450342536",
+    "url": "/drum-machine/static/js/main.40bac8ec.chunk.js"
+  },
+  {
+    "revision": "d7d1943964fcc80e9287",
+    "url": "/drum-machine/static/js/runtime-main.49a0ba3c.js"
+  }
+]);
